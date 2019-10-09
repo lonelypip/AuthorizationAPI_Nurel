3bdd42faa48a2d1766f92373e2b081456f99d680
Daryn <kapa.miki16@gmail.com>
2019-10-09 22:53:48 +0600

    Corsheaders

--- AUTH_API/AUTH_API/settings.py
+++ AUTH_API/AUTH_API/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',
+    'corsheaders',
     #apps
     'apps.auth_app'
 ]
@@ -123,4 +124,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+CORS_ORIGIN_ALLOW_ALL = True
 
